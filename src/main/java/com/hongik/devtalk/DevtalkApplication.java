542473c5ab6418c880c1b6efb6246b1895ada737
soyeoneeii <syp0463@gmail.com>
2025-09-15 17:10:52 +0900

    [Chore] 프로젝트 생성

---
+++ src/main/java/com/hongik/devtalk/DevtalkApplication.java
@@ -0,0 +1,13 @@
+package com.hongik.devtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongik/devtalk/DevtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongik.devtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
