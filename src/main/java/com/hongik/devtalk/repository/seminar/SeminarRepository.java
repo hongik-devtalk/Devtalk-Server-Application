3a497e3331bb2facfe5a82a1351974fd05c3815e
shinae <shinae1023@naver.com>
2025-09-29 13:12:06 +0900

    [Feat] 머지용

--- src/main/java/com/hongik/devtalk/repository/seminar/SeminarRepository.java
+++ src/main/java/com/hongik/devtalk/repository/seminar/SeminarRepository.java
@@ -1,11 +1,11 @@
 package com.hongik.devtalk.repository.seminar;
 
 import com.hongik.devtalk.domain.Seminar;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface SeminarRepository extends JpaRepository<Seminar,Long> {
+
     List<Seminar> findAllByOrderBySeminarNumDesc();
 }
--- src/main/java/com/hongik/devtalk/repository/seminar/StudentRepository.java
+++ src/main/java/com/hongik/devtalk/repository/seminar/StudentRepository.java
@@ -4,4 +4,5 @@ import com.hongik.devtalk.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    Student findByStudentNum(String studentNum);
 }
