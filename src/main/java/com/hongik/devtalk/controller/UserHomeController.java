6b662ab092f08638f69f05117248c8c4df50d803
soyeoneeii <syp0463@gmail.com>
2025-10-01 00:41:41 +0900

    [Refactor] swagger anotation 수정

--- src/main/java/com/hongik/devtalk/controller/UserHomeController.java
+++ src/main/java/com/hongik/devtalk/controller/UserHomeController.java
@@ -15,16 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/user/home")
-@Tag(name="유저 홈화면")
+@Tag(name="[User] MainPage",description = "유저 홈화면 관련 API")
 public class UserHomeController {
 
     private final SeminarInfoService seminarInfoService;
 
     @GetMapping("/{seminarId}")
     @Operation(summary = "현재 신청받고 있는 세미나 정보를 보여줍니다.")
-    public ResponseEntity<ApiResponse<SeminarInfoResponseDTO>> getSeminar(@PathVariable Long seminarId){
+    public ResponseEntity<ApiResponse<SeminarInfoResponseDTO>> getSeminar(@PathVariable Long seminarId) {
         SeminarInfoResponseDTO result = seminarInfoService.getSeminarById(seminarId);
-        return ResponseEntity.ok(ApiResponse.onSuccess("",result));
+        return ResponseEntity.ok(ApiResponse.onSuccess("", result));
     }
 
 }
