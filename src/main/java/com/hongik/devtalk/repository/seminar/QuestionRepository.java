8c8eb3350de76df7f1847e80de7d4c7a9ca88537
yujeong <parkyj0430@gmail.com>
2025-10-01 02:24:11 +0900

    [Feat] 세미나 연사별 질문 조회 API 구현

---
+++ src/main/java/com/hongik/devtalk/repository/seminar/QuestionRepository.java
@@ -0,0 +1,18 @@
+package com.hongik.devtalk.repository.seminar;
+
+import com.hongik.devtalk.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    @Query("select distinct q from Question q " +
+            "join fetch q.student s " +
+            "join fetch q.session ss " +
+            "join fetch ss.speaker sp " +
+            "join fetch ss.seminar se " +
+            "where se.id = :seminarId")
+    List<Question> findQuestionsBySeminarId(Long seminarId);
+}
