99bf9232ef0c2fffde0e2d25e454a4cb76e7610d
soyeoneeii <syp0463@gmail.com>
2025-10-01 01:53:00 +0900

    [Refactor] 2차 개발 명시

--- src/main/java/com/hongik/devtalk/controller/userhome/UserHomeController.java
+++ src/main/java/com/hongik/devtalk/controller/userhome/UserHomeController.java
@@ -32,13 +32,13 @@ public class UserHomeController {
 
     // 2차 개발 사항
     @GetMapping("/speaker/{seminarId}")
-    @Operation(summary = "해당 세미나 연사 리스트 조회")
+    @Operation(summary = "[2차] 해당 세미나 연사 리스트 조회")
     public ResponseEntity<ApiResponse<SpeakerListResponseDTO>> getSpeakerList(@PathVariable Long seminarId){
         return ResponseEntity.ok().build(); // 임시
     }
 
     @GetMapping("/review/{seminarId}")
-    @Operation(summary = "해당 세미나 후기 리스트 조회")
+    @Operation(summary = "[2차] 해당 세미나 후기 리스트 조회")
     public ResponseEntity<ApiResponse<ReviewListResponseDTO>> getReviewList(@PathVariable Long seminarId){
         return ResponseEntity.ok().build(); // 임시
     }
