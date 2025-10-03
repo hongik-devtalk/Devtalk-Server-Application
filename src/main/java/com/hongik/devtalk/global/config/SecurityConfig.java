69ea39cbceebfe802d18419a8fa0b4a4318ad584
nsh0919 <chelsea4570@naver.com>
2025-10-04 05:11:00 +0900

    [Fix] 스웨거 문서 cors 경로 추가

--- src/main/java/com/hongik/devtalk/global/config/SecurityConfig.java
+++ src/main/java/com/hongik/devtalk/global/config/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig {
         var config = new org.springframework.web.cors.CorsConfiguration();
         config.setAllowedOrigins(java.util.List.of(
                 "http://localhost:5173",
-                "https://hongikdevtalk.com"
+                "https://hongikdevtalk.com",
+                "https://api.hongikdevtalk.com"
         ));
         config.setAllowedMethods(java.util.List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(java.util.List.of("*"));
