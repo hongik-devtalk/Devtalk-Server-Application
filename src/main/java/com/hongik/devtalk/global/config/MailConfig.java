15f7bb04df7c6f815a2e6e0c6fd5b429157200d9
soyeoneeii <syp0463@gmail.com>
2025-10-03 16:00:45 +0900

    [Fix] gmail로 모두 변경 및 value로 가져오기

--- src/main/java/com/hongik/devtalk/global/config/MailConfig.java
+++ src/main/java/com/hongik/devtalk/global/config/MailConfig.java
@@ -4,34 +4,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Properties;
 
 @Configuration
 public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
-        javaMailSender.setHost("smtp.naver.com");
-        javaMailSender.setUsername("네이버 SMTP 설정 이메일");
-        javaMailSender.setPassword("네이버 계정 비밀번호");
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
 
-        javaMailSender.setPort(465);
-
-        javaMailSender.setJavaMailProperties(getMailProperties());
-
-        return javaMailSender;
-    }
-
-    private Properties getMailProperties() {
         Properties properties = new Properties();
         properties.setProperty("mail.transport.protocol", "smtp");
         properties.setProperty("mail.smtp.auth", "true");
         properties.setProperty("mail.smtp.starttls.enable", "true");
         properties.setProperty("mail.debug", "true");
-        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
-        properties.setProperty("mail.smtp.ssl.enable","true");
-        return properties;
+
+        javaMailSender.setJavaMailProperties(properties);
+
+        return javaMailSender;
     }
 }
