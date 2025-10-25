0becc3bf28594aad3eec506e0add11a61d5b74ff
soyeoneeii <syp0463@gmail.com>
2025-10-26 01:36:39 +0900

    [Fix] ShowSeminar 엔티티 수정

--- src/main/java/com/hongik/devtalk/domain/ShowSeminar.java
+++ src/main/java/com/hongik/devtalk/domain/ShowSeminar.java
@@ -3,22 +3,36 @@ package com.hongik.devtalk.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class ShowSeminar { // 홈화면 노출 세미나
+public class ShowSeminar { // single row entity
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long showSeminarNum;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seminar_id", nullable = true)
+    private Seminar seminar;
 
     // 신청 활성화
     private boolean applicantActivate;
 
     // Live 활성화
     private boolean LiveActivate;
+
+    // 가장 최근에 업데이트 된 정보를 기준으로 조회하기
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    @PreUpdate
+    public void updateTimestamp() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
 }
