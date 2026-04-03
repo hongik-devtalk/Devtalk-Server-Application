6b3f875fc8543685eb94c6ae79917171d3cedcc3
양지윤 <yangjiyun748@gmail.com>
2026-04-03 11:16:49 +0900

    fix: 파일 누락 문제 해결

--- src/main/java/com/hongik/devtalk/service/seminar/AdminStatsService.java
+++ src/main/java/com/hongik/devtalk/service/seminar/AdminStatsService.java
@@ -8,7 +8,6 @@ import com.hongik.devtalk.repository.seminar.SeminarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Locale;
