aed1c2ee0cc03d6fe46a270503e32daf4d357054
황신애 <shinae1023@naver.com>
2026-04-04 20:17:54 +0900

    SeminarApplicantController.java 업데이트

--- src/main/java/com/hongik/devtalk/controller/seminar/SeminarApplicantController.java
+++ src/main/java/com/hongik/devtalk/controller/seminar/SeminarApplicantController.java
@@ -11,10 +11,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
