b8e606730c5272cf0c719cefa216b2b118cb9850
shinae1023 <shinae1023@naver.com>
2026-04-19 21:30:15 +0900

    [Feat] 서버 시간대 한국으로 변경

--- src/main/java/com/hongik/devtalk/DevtalkApplication.java
+++ src/main/java/com/hongik/devtalk/DevtalkApplication.java
@@ -1,16 +1,22 @@
 package com.hongik.devtalk;
 
-import org.springframework.boot.SpringApplication;
+import jakarta.annotation.PostConstruct;import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableScheduling;import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class DevtalkApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DevtalkApplication.class, args);
 	}
 
